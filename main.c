616e438368a8da62fa89fdf9ac7de75805806fb9
53p3h2 <53p3h2@gmail.com>
2025-03-17 18:48:43 +0330

    adding arguments strings parser function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct arguments
 {
@@ -7,9 +8,27 @@ typedef struct arguments
     char **files;
 } arguments;
 
+void parse_arguments(int argc, char **argv, arguments *args){
+    args->files = malloc(argc * sizeof(char*));
+    int index = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--help") == 0)
+        {
+            printf("Usage: %s file1 file2 [file]...\n", argv[0]);
+            exit(0);
+        }else
+        {
+            args->files[index] = argv[i];
+            index++;
+        }
+    }
+    args->files_count = index;
+}
+
 int main(int argc, char *argv[]) {
-
-
-
+    arguments args;
+    parse_arguments(argc, argv, &args);
+    printf("Files count: %d\n", args.files_count);
     return 0;
 }
