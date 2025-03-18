22e5d7fdb650a653925425bf4d5575e74915a55a
53p3h2 <53p3h2@gmail.com>
2025-03-18 15:48:38 +0330

    adding file_read function

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INITIAL_BUFFER_SIZE 128
+
 typedef struct arguments
 {
     unsigned int files_count;
     char **files;
 } arguments;
 
-void parse_arguments(int argc, char **argv, arguments *args){
-    args->files = malloc(argc * sizeof(char*));
+void parse_arguments(int argc, char **argv, arguments *args)
+{
+    args->files = malloc(argc * sizeof(char *));
     int index = 0;
     for (int i = 1; i < argc; i++)
     {
@@ -17,7 +20,8 @@ void parse_arguments(int argc, char **argv, arguments *args){
         {
             printf("Usage: %s file1 file2 [file]...\n", argv[0]);
             exit(0);
-        }else
+        }
+        else
         {
             args->files[index] = argv[i];
             index++;
@@ -26,9 +30,54 @@ void parse_arguments(int argc, char **argv, arguments *args){
     args->files_count = index;
 }
 
-int main(int argc, char *argv[]) {
+void read_file(char *path, char **buffer)
+{
+    FILE *f = fopen(path, "r");
+    if (f == NULL)
+    {
+        perror("File opening failed");
+        *buffer = NULL;
+        return;
+    }
+    int tmp_capacity = INITIAL_BUFFER_SIZE;
+    int tmp_size = 0;
+    char *tmp = malloc(tmp_capacity * sizeof(char));
+    if (tmp == NULL) {
+        perror("Malloc failed");
+        *buffer = NULL;
+        fclose(f);
+        return;
+    }
+    size_t bytes_read;
+    while ((bytes_read = fread(tmp + tmp_size, 1, tmp_capacity - tmp_size, f)) > 0)
+    {
+        if (tmp_size + bytes_read >= tmp_capacity)
+        {
+            tmp_capacity *= 2;
+            tmp = realloc(tmp, tmp_capacity * sizeof(char));
+            if (tmp == NULL)
+            {
+                perror("Realloc failed");
+                *buffer = NULL;
+                fclose(f);
+                return;
+            }
+            
+        }
+        tmp_size += bytes_read;
+    }
+    tmp[tmp_size] = '\0';
+    *buffer = tmp;
+    fclose(f);
+}
+
+int main(int argc, char *argv[])
+{
     arguments args;
     parse_arguments(argc, argv, &args);
     printf("Files count: %d\n", args.files_count);
+    char *buffer = NULL;
+    read_file(args.files[0], &buffer);
+    printf("%s\n", buffer);
     return 0;
 }
